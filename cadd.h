385551ac2f01a5909c2be798a3db884dfa09e495
JD-Dilakshan <viknarajadilakshan@gmail.com>
2023-10-13 11:20:51 +0530

    Account Create

--- cadd.h
+++ cadd.h
@@ -13,44 +13,73 @@ float Create_Account()
     string email;
     string nic;
     int dob;
-    string cs;
-
+    int ans;
+do{
     fstream CusFile("customer.txt",ios::app);
     cout<<"\n Enter the account number: ";
     cin>>account;
-    CusFile<<" account:"<<account;
+    CusFile<<account << " ";
     cout<<"\n Enter the account holder name: ";
     cin>>name;
-    CusFile<<" name:"<<name;
+    CusFile<<name << " ";
     cout<<"\n Enter the account type: ";
     cin>>type;
-    CusFile<<" type:"<<type;
+    CusFile<<type << " ";
     cout<<"\n Enter your address: ";
     cin>>address;
-    CusFile<<" address:"<<address;
+    CusFile<<address<<" ";
     cout<<"\n Enter your phone number: ";
     cin>>phone;
-    CusFile<<" phone:"<<phone;
+    CusFile<<phone<<" ";
     cout<<"\n Enter your email address: ";
     cin>>email;
-    CusFile<<" email:"<<email;
+    CusFile<<email <<" ";
     cout<<"\n Enter your nic number: ";
     cin>>nic;
-    CusFile<<" nic:"<<nic;
+    CusFile<<nic <<" ";
     cout<<"\n Enter your DOB: ";
     cin>>dob;
-    CusFile<<" dob:"<<dob;
+    CusFile<<dob <<" ";
     cout<<"\n Enter your initial amount: ";
     cin>>initial;
-    CusFile<<" initial:"<<initial;
+    CusFile<<initial <<" "<<endl;
     CusFile.close();
 
 
     cout<<"\n Account Created....***";
+
+    cout<<" Choose Option"<<endl;
+    cout<<" 1 Create another account"<<endl;
+    cout<<" 2 Back to main menu"<<endl;
+    cout<<" 3 Logout"<<endl;
+    cout<<" 4 Exit"<<endl;
+
+    cin>>ans;
+
 }
 
+    while (ans == 1);
+    switch (ans)
+
+    {
+
+    case 2:
+        return 0;
+        break;
+
+    case 3:
+        return 0;
+        break;
+
+    case 4:
+        return 0;
+        break;
+
+    default:
+        return 100;
 
 
+    }
 
 
-
+}
