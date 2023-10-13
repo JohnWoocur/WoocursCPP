1c30c0e00f46122e119d0bec4d3cc11c2d57391c
linthu <linthujan99@gmail.com>
2023-10-13 21:48:25 +0530

    set menu switch with all functions

--- cadd.h
+++ cadd.h
@@ -14,49 +14,49 @@ float Create_Account()
     string nic;
     int dob;
     int ans;
-do{
-    fstream CusFile("customer.txt",ios::app);
-    cout<<"\n Enter the account number: ";
-    cin>>account;
-    CusFile<<account << " ";
-    cout<<"\n Enter the account holder name: ";
-    cin>>name;
-    CusFile<<name << " ";
-    cout<<"\n Enter the account type: ";
-    cin>>type;
-    CusFile<<type << " ";
-    cout<<"\n Enter your address: ";
-    cin>>address;
-    CusFile<<address<<" ";
-    cout<<"\n Enter your phone number: ";
-    cin>>phone;
-    CusFile<<phone<<" ";
-    cout<<"\n Enter your email address: ";
-    cin>>email;
-    CusFile<<email <<" ";
-    cout<<"\n Enter your nic number: ";
-    cin>>nic;
-    CusFile<<nic <<" ";
-    cout<<"\n Enter your DOB: ";
-    cin>>dob;
-    CusFile<<dob <<" ";
-    cout<<"\n Enter your initial amount: ";
-    cin>>initial;
-    CusFile<<initial <<" "<<endl;
-    CusFile.close();
+    do
+    {
+        fstream CusFile("customer.txt", ios::app);
+        cout << "\n Enter the account number: ";
+        cin >> account;
+        CusFile << account << " ";
+        cout << "\n Enter the account holder name: ";
+        cin >> name;
+        CusFile << name << " ";
+        cout << "\n Enter the account type: ";
+        cin >> type;
+        CusFile << type << " ";
+        cout << "\n Enter your address: ";
+        cin >> address;
+        CusFile << address << " ";
+        cout << "\n Enter your phone number: ";
+        cin >> phone;
+        CusFile << phone << " ";
+        cout << "\n Enter your email address: ";
+        cin >> email;
+        CusFile << email << " ";
+        cout << "\n Enter your nic number: ";
+        cin >> nic;
+        CusFile << nic << " ";
+        cout << "\n Enter your DOB: ";
+        cin >> dob;
+        CusFile << dob << " ";
+        cout << "\n Enter your initial amount: ";
+        cin >> initial;
+        CusFile << initial << " " << endl;
+        CusFile.close();
 
+        cout << "\n Account Created....***";
 
-    cout<<"\n Account Created....***";
+        cout << " Choose Option" << endl;
+        cout << " 1 Create another account" << endl;
+        cout << " 2 Back to main menu" << endl;
+        cout << " 3 Logout" << endl;
+        cout << " 4 Exit" << endl;
 
-    cout<<" Choose Option"<<endl;
-    cout<<" 1 Create another account"<<endl;
-    cout<<" 2 Back to main menu"<<endl;
-    cout<<" 3 Logout"<<endl;
-    cout<<" 4 Exit"<<endl;
+        cin >> ans;
 
-    cin>>ans;
-
-}
+    }
 
     while (ans == 1);
     switch (ans)
@@ -77,9 +77,5 @@ do{
 
     default:
         return 100;
-
-
     }
-
-
 }
